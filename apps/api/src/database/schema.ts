89fa886a2dee6431bcd3a8ce464c94b04bfa2c57
Arjan <ahoekdev@gmail.com>
2026-03-29 20:39:33 +0200

    add lodges schema

--- apps/api/src/database/schema.ts
+++ apps/api/src/database/schema.ts
@@ -1,6 +1,9 @@
+export { lodges } from './schemas/lodges.schema';
 export { users } from './schemas/users.schema';
+import { lodges } from './schemas/lodges.schema';
 import { users } from './schemas/users.schema';
 
 export const schema = {
+  lodges,
   users,
 };
---
+++ apps/api/src/database/schemas/lodges.schema.ts
@@ -0,0 +1,12 @@
+import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
+import { users } from './users.schema';
+
+export const lodges = pgTable('lodges', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull().unique(),
+  country: text('country').notNull(),
+  keeperId: uuid('keeper_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'restrict' }),
+  slug: text('slug').notNull().unique(),
+});
