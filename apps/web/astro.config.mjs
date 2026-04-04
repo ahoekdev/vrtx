3b5ecf95903d530b2819c7d68f6200c33ebb404e
Arjan <ahoekdev@gmail.com>
2026-04-04 08:49:19 +0200

    configure tailwind

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({ adapter: node({ mode: "standalone" }) });
+export default defineConfig({
+  adapter: node({ mode: "standalone" }),
+  vite: { plugins: [tailwindcss()] },
+});
