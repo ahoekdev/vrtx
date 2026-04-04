d76c03f90eb064440aaf0edac1ca7b828e44a571
Arjan <ahoekdev@gmail.com>
2026-04-04 09:21:11 +0200

    add react plugin

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({ mode: "standalone" }),
   vite: { plugins: [tailwindcss()] },
+  integrations: [react()],
 });
