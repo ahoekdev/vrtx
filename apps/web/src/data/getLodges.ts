e8990ef98a985c2baaa2f18914308ecf16a0f13b
Arjan <ahoekdev@gmail.com>
2026-04-04 08:44:15 +0200

    strip styling and create separate helper for fetching lodges

---
+++ apps/web/src/data/getLodges.ts
@@ -0,0 +1,17 @@
+import type { Lodge } from "../types/Lodge";
+import { getApiBaseUrl } from "../utils/getApiBaseUrl";
+
+export async function getLodges() {
+  try {
+    const res = await fetch(new URL("/lodges", getApiBaseUrl()));
+
+    if (!res.ok) {
+      return { data: [], error: true };
+    } else {
+      const data = (await res.json()) as Lodge[];
+      return { data, error: false };
+    }
+  } catch {
+    return { data: [], error: true };
+  }
+}
