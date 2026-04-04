7e3497763f513ce24cf36570d7c1e6c5b74c62b2
Arjan <ahoekdev@gmail.com>
2026-04-04 08:28:55 +0200

    remove web core ui stuff

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from "astro/config";
-import { webcore } from "webcoreui/integration";
 import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
-  integrations: [webcore()],
-});
+export default defineConfig({ adapter: node({ mode: "standalone" }) });
