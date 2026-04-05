009248a81064208eb8012f70f6864c2c8ec0f7a5
Arjan <ahoekdev@gmail.com>
2026-04-05 11:23:53 +0200

    add lodge delete action

--- apps/web/src/actions/index.ts
+++ apps/web/src/actions/index.ts
@@ -1,3 +1,4 @@
 import { createLodge } from "./lodges/createLodge.action";
+import { deleteLodge } from "./lodges/deleteLodge.action";
 
-export const server = { createLodge };
+export const server = { createLodge, deleteLodge };
