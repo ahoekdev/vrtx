931519e7dededacb315fe2233fb55cc5577f64df
Arjan <ahoekdev@gmail.com>
2026-05-03 13:10:29 +0200

    update api port listen config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ import { RequestValidationPipe } from './pipes/request-validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new RequestValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '::');
 }
 
 void bootstrap();
