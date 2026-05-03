333c50e7fff61949ec591e64f36118c161436cb6
Arjan <ahoekdev@gmail.com>
2026-05-03 12:58:36 +0200

    log base url

--- apps/web/src/data/getLodges.ts
+++ apps/web/src/data/getLodges.ts
@@ -2,6 +2,7 @@ import type { Lodge } from "../types/Lodge";
 import { getApiBaseUrl } from "../utils/getApiBaseUrl";
 
 export async function getLodges() {
+  console.log(getApiBaseUrl());
   try {
     const res = await fetch(new URL("/lodges", getApiBaseUrl()));
 
